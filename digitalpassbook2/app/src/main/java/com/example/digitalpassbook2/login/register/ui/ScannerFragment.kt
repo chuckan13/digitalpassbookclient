33dee1237748a4dc471346300b0ad8fa81672433
Devin Plumb <dplumb@princeton.edu>
2020-05-03 00:18:04 -0400

    tinker

--- digitalpassbook2/app/src/main/java/com/example/digitalpassbook2/login/register/ui/ScannerFragment.kt
+++ digitalpassbook2/app/src/main/java/com/example/digitalpassbook2/login/register/ui/ScannerFragment.kt
@@ -44,6 +44,5 @@ class ScannerFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ScannerViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 }
