854cf5ff0680fc315831a4bcc8101cb83e6cd361
Justin Curl <jcurl@princeton.edu>
2020-05-07 01:35:57 -0700

    removed awful button rising with keyboard

--- digitalpassbook2/app/src/main/java/com/example/digitalpassbook2/server/Pass.kt
+++ digitalpassbook2/app/src/main/java/com/example/digitalpassbook2/server/Pass.kt
@@ -1,7 +1,10 @@
 package com.example.digitalpassbook2.server
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 class Pass(
     @field:SerializedName("orgId") val orgId: Int,
     @field:SerializedName("userId") var userId: Int,
@@ -9,7 +12,7 @@ class Pass(
     @field:SerializedName("date") var date: String,
     @field:SerializedName("owners") var owners: Array<String>,
     @field:SerializedName("isLocked") var isLocked: Boolean
-) {
+): Parcelable {
     @SerializedName("id")
     val id = 0
 }
